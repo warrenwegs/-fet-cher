70201a456201dd262fb08cd9d14dfc2b7eb255ed
warrenwegs <warrenwegner@gmail.com>
2015-10-08 19:36:46 +0000

    finished slideshow js

--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,12 @@ require 'pry' if development?
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
 
+
+
 configure do
+
+  set :instagram_key, '2227106820.1fb234f.914b7e6a1e114dbebe0b2a2952d51f1a&'
+
   set :root, APP_ROOT.to_path
   set :server, :puma
 
